c13a17a59a67bd41fdb184781869780f5fcab45a
Matt Venables <matt@venabl.es>
2025-01-07 06:02:37 -0500

    Add tsup for builds

---
+++ tsup.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig([
+  {
+    entry: ["src/index.ts"],
+    outDir: "dist",
+    format: ["cjs", "esm"],
+    clean: true,
+    dts: true,
+    sourcemap: true
+  }
+])
